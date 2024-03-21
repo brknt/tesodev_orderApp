7f9d59cee30b33e9e94a0a127a9a580aff6272bf
brknt <brknt.gns@hotmail.com>
2024-03-22 02:09:55 +0300

    create added for Customer operations test

--- product/test/productContoller.test.js
+++ product/test/productContoller.test.js
@@ -10,13 +10,13 @@ const expect = chai.expect;
 
 describe('Admin and Customer Product Operations', () => {
 
-    describe('Admin operations', () => {
+    describe('Admin operations for PRODUCT', () => {
         after(async () => {
             await chai
                 .request("http://127.0.0.1:8081")
                 .get('/logout');
-            await Product.deleteMany({ _id: productId });
-            console.log('test product deleted');
+            await Product.deleteMany({ _id: adminProductId });
+            console.log('[admin]test product deleted');
         });
         before(async () => {
 
@@ -59,7 +59,7 @@ describe('Admin and Customer Product Operations', () => {
                     .post('/create')
                     .send(product)
                     .set('Cookie', `jwt=${tokenAdmin}`);
-                productId = res.body.data.result._id;
+                adminProductId = res.body.data.result._id;
                 expect(res).to.have.status(201);
                 expect(res.body).to.be.a('object');
                 expect(res.body).to.have.property('code');
@@ -95,7 +95,7 @@ describe('Admin and Customer Product Operations', () => {
                         cityCode: 1233
                     },
                     ids: [
-                        { _id: productId },
+                        { _id: adminProductId },
                     ]
                 };
 
@@ -113,5 +113,113 @@ describe('Admin and Customer Product Operations', () => {
 
 
     });
+    describe('Customer operations for PRODUCT', () => {
+        var customerId;
+        after(async () => {
+            await chai
+                .request("http://127.0.0.1:8081")
+                .get('/logout');
+            await Product.deleteMany({ _id: customerProductId });
+            console.log('[customer]test product deleted');
+
+            await chai
+                .request("http://127.0.0.1:8081")
+                .post('/login')
+                .send({
+                    email: "admin@gmail.com",
+                    password: "admin"
+                    
+                }).then((res)=>{
+                    tokenAdmin2 = res.body.data.token;
+                    expect(res).to.have.status(200);
+                })
+
+            await chai
+                .request("http://127.0.0.1:8081")
+                .delete(`/delete/${customerId}`)
+                .set('Cookie', `jwt=${tokenAdmin2}`)
+                .then((res) => {
+                    expect(res).to.have.status(200);
+                })
+
+        });
+        before(async () => {
+
+            const customer = {
+                name: "customerproducttest@gmail.com",
+                email: "customerproducttest@gmail.com",
+                password: "test1234",
+                address: {
+                    addressLine: "line",
+                    city: "customertestcity",
+                    country: "custumertestcountry",
+                    cityCode: 1
+                }
+            };
+
+            await chai
+                .request("http://127.0.0.1:8081")
+                .post('/create')
+                .send(customer)
+                .then((res) => {
+                    customerId = res.body.data.result;
+                    console.log('IDDD::', customerId);
+
+                    expect(res).to.have.status(201);
+                });
+
+            await chai
+                .request("http://127.0.0.1:8081")
+                .post('/login')
+                .send({
+                    email: customer.email,
+                    password: customer.password
+                })
+                .then((res) => {
+                    tokenCustomer = res.body.data.token;
+                    expect(res, "customer login error:").to.have.status(200);
+
+                }).catch((err) => {
+                    if (err) {
+                        console.log('Customer service is not up!');
+                    }
+                })
+
+
+
+
+
+
+        });
+
+        describe('POST /create', () => {
+            it('[Customer] should create a new product', async () => {
+                const product = {
+                    name: "customerProduct1",
+                    price: 45.2,
+                    imageUrl: "customerurl1"
+                };
+                const res = await chai
+                    .request(app)
+                    .post('/create')
+                    .send(product)
+                    .set('Cookie', `jwt=${tokenCustomer}`);
+                customerProductId = res.body.data.result._id;
+                expect(res).to.have.status(201);
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.have.property('code');
+                expect(res.body).to.have.property('data');
+                expect(res.body.data).to.have.property('success', true);
+                expect(res.body.data).to.have.property('result');
+
+
+
+            });
+        });
+
+
+    });
+
+
 
 });
